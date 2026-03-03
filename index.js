3b86770579db9f2b309f28ab30709983a001b85a
lance-l0t <sliceadice72@gmail.com>
2026-03-03 19:05:00 +0300

    Updated Javascript and UI elements

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const darkBtn = document.querySelector('.dark');
 const body = document.body;
-const transactionContainer = document.querySelector('.transaction-history');
+const inputContainer = document.querySelector('.input-container');
+const inputMenu = document.querySelector('.input-menu');
 
 let addvalue = 0;
-const addHistoryBtn = document.getElementById('add-expenditure');
+const submitBtn = document.querySelector('#add-expenditure');
+const selection = document.getElementById('choices');
+const details = document.getElementById('details');
+const amount = document.getElementById('amount');
+const balance = document.getElementById('balance');
 
+const category = document.querySelector('.first-transaction category');
+const detailsContainer = document.querySelector('.first-transaction details');
+const amountContainer = document.querySelector('.first-transaction amount');
+const balanceContainer = document.querySelector('.first-transaction balance');
 
-
+const containers = [category , detailsContainer , amountContainer , balanceContainer];
 
 let i = localStorage.getItem('state');
-console.log('local storage = ',i);
 
 if (i % 2 != 0) {
     body.classList.toggle('dark-mode');
@@ -27,3 +35,43 @@ darkBtn.addEventListener('click', function(){
         darkBtn.innerHTML = '<i class="fa-solid fa-sun"></i> Light';
     }
 })
+
+function submitHistoryInfo(){
+    inputContainer.style.display = 'block';
+    inputContainer.style.opacity = '1';
+}
+
+
+function addHistoryInfo(){
+
+    const selectionChoice = selection.value;
+    const additionalInfo = details.value;
+    const amountSpent = amount.value;
+    const remainingBalance = balance.value;
+
+    const inputValues = [selectionChoice , additionalInfo, amountSpent , remainingBalance];
+
+    if (additionalInfo == '' && amountSpent == '' && remainingBalance == '') {
+        let errorMessage = document.createElement('h6');
+        let timer = setInterval(function(){
+            errorMessage.textContent = 'Fill all the inputs...';
+            inputMenu.append(errorMessage);
+        }, 1000);
+
+        setTimeout(function(){
+            clearInterval(timer);
+            errorMessage.remove()
+        }, 5000);
+        
+
+    } else {
+        inputContainer.style.display = 'none';
+        updateHistory();
+        
+}
+}
+
+function updateHistory(){
+    let tag = document.createElement('h5');
+
+}
