2ea7c34a55fa753727fca3257358adcb048a7ded
lancelot <sliceadice@gmail.com>
2026-04-16 20:16:09 +0300

    Removed summary bar and updated javascript

--- index.js
+++ index.js
@@ -1,112 +1,145 @@
-const darkBtn = document.querySelector('.dark');
+const darkBtn = document.querySelector(".dark");
 const body = document.body;
-const inputContainer = document.querySelector('.input-container');
-const inputMenu = document.querySelector('.input-menu');
+const inputContainer = document.querySelector(".input-container");
+const inputMenu = document.querySelector(".input-menu");
 
 let addvalue = 0;
-const submitBtn = document.querySelector('#add-expenditure');
+const submitBtn = document.querySelector("#add-expenditure");
 
+const category = document.querySelector(".first-transaction category");
+const detailsContainer = document.querySelector(".first-transaction details");
+const amountContainer = document.querySelector(".first-transaction amount");
 
-const category = document.querySelector('.first-transaction category');
-const detailsContainer = document.querySelector('.first-transaction details');
-const amountContainer = document.querySelector('.first-transaction amount');
+const cont = document.querySelector(".input-container");
+const cancelBtn = document.getElementById("cancelBtn");
 
+let currentBalance = document.getElementById("balance-display");
 
-const cont = document.querySelector('.input-container');
-const cancelBtn = document.getElementById('cancelBtn');
+const addBalanceBtn = document.querySelector(".add-btn");
+const depositContainer = document.querySelector(".deposit-container");
 
-// let contain = [];
-// let inputValues = [];
-// const remainingBalance = balance.value;
+const depositAmount = document.getElementById("depositAmount");
+const depositInfo = document.getElementById("depositInfo");
 
+let balance = Number(currentBalance.value);
 
+let contain = [
+  document.querySelector(".first-transaction.category"),
+  document.querySelector(".first-transaction.details"),
+  document.querySelector(".first-transaction.amount"),
+];
 
-
-let i = localStorage.getItem('state');
+let i = localStorage.getItem("state");
 
 if (i % 2 != 0) {
-    body.classList.toggle('dark-mode');
+  body.classList.toggle("dark-mode");
 }
-darkBtn.addEventListener('click', function () {
-
-    i++;
-    localStorage.setItem('state', i);
-    console.log(i);
-    if (i % 2 == 0) {
-        body.classList.toggle('dark-mode');
-        darkBtn.innerHTML = '<i class="fa-solid fa-moon"></i> Dark';
-    } else {
-        body.classList.toggle('dark-mode');
-        darkBtn.innerHTML = '<i class="fa-solid fa-sun"></i> Light';
-    }
-})
+darkBtn.addEventListener("click", function () {
+  i++;
+  localStorage.setItem("state", i);
+  console.log(i);
+  if (i % 2 == 0) {
+    body.classList.toggle("dark-mode");
+    darkBtn.innerHTML = '<i class="fa-solid fa-moon"></i> Dark';
+  } else {
+    body.classList.toggle("dark-mode");
+    darkBtn.innerHTML = '<i class="fa-solid fa-sun"></i> Light';
+  }
+});
 
 function submitHistoryInfo() {
-    inputContainer.style.display = 'block';
-    inputContainer.style.opacity = '1';
+  inputContainer.style.display = "block";
+  inputContainer.style.opacity = "1";
+  inputMenu.style.display = "block";
+  depositContainer.style.display = "none";
 }
-
 
 function addHistoryInfo() {
-    let selection = document.getElementById('choices');
-    let details = document.getElementById('details');
-    let amount = document.getElementById('amount');
+  let selection = document.getElementById("choices");
+  let details = document.getElementById("details");
+  let amount = document.getElementById("amount");
 
-    let selectionChoice = selection.value;
-    let additionalInfo = details.value;
-    let amountSpent = amount.value;
+  let selectionChoice = selection.value;
+  let additionalInfo = details.value;
+  let amountSpent = amount.value;
 
+  let inputValues = [selectionChoice, additionalInfo, amountSpent];
+  if (additionalInfo == "" || amountSpent == "") {
+    let errorMessage = document.createElement("h6");
+    console.log(selectionChoice, amountSpent, additionalInfo);
+    let timer = setInterval(function () {
+      errorMessage.textContent = "Fill all the inputs...";
+      inputMenu.append(errorMessage);
+    }, 1000);
 
-    let contain = [document.querySelector('.first-transaction.category'), document.querySelector('.first-transaction.details'), document.querySelector('.first-transaction.amount')];
+    setTimeout(function () {
+      clearInterval(timer);
+      errorMessage.remove();
+    }, 5000);
+  } else {
+    let k = 0;
 
+    if (balance >= amountSpent) {
+      balance -= amountSpent;
 
-    let inputValues = [selectionChoice, additionalInfo, amountSpent];
-    if (additionalInfo == '' || amountSpent == '') {
-        let errorMessage = document.createElement('h6');
-        console.log(selectionChoice, amountSpent, additionalInfo)
-        let timer = setInterval(function () {
-            errorMessage.textContent = 'Fill all the inputs...';
-            inputMenu.append(errorMessage);
-        }, 1000);
-
-        setTimeout(function () {
-            clearInterval(timer);
-            errorMessage.remove()
-        }, 5000);
-
-
+      contain.forEach((container) => {
+        let tag = document.createElement("h5");
+        console.log(container, inputValues[k]);
+        tag.style.paddingBlock = "5px";
+        if (Number(inputValues[k])) {
+          tag.textContent = "$" + String(inputValues[k]);
+        } else {
+          tag.textContent = inputValues[k];
+        }
+        container.append(tag);
+        cont.style.display = "none";
+        tag.style.color = "red";
+        k++;
+      });
+      inputContainer.style.display = "none";
+      selectionChoice = "Personal";
+      additionalInfo = "";
+      amountSpent = "";
     } else {
-        inputContainer.style.display = 'none';
-        selectionChoice = '';
-        additionalInfo = '';
-        amountSpent = '';
-
-        let k = 0;
-        contain.forEach(container => {
-            
-            let tag = document.createElement('h5');
-            console.log(container, inputValues[k]);
-            tag.style.paddingBlock = '10px';
-            tag.textContent = inputValues[k];
-            container.append(tag);
-            cont.style.display = 'none';
-            k++;
-        });
+      alert("Insufficient Balance");
     }
+  }
 }
-
 
 // Add balance button
 
-const addBalanceBtn = document.querySelector('.add-btn');
-addBalanceBtn.addEventListener('mouseclick', function () {
-
-    cont.style.display = 'block';
-})
-
+function topUp() {
+  depositContainer.style.display = "block";
+  cont.style.display = "block";
+  inputMenu.style.display = "none";
+}
 
 //Exit input container
 
 function cancel() {
-    cont.style.display = 'none';
+  selectionChoice = "Personal";
+  additionalInfo = "";
+  amountSpent = "";
+  cont.style.display = "none";
+}
+
+function updateBalance() {
+  console.log(balance)
+  balance += Number(depositAmount.value);
+  cont.style.display = "none";
+  currentBalance.textContent = `$${balance}`;
+
+  let info = ["Deposit", depositInfo.value, String(depositAmount.value)];
+  let p = 0;
+  contain.forEach((element) => {
+    let balanceDisplay = document.createElement("h5");
+    if (Number(info[p])) {
+      balanceDisplay.textContent = `$${info[p]}`;
+    } else {
+      balanceDisplay.textContent = info[p];
+    }
+    balanceDisplay.style.color = "lime";
+    element.append(balanceDisplay);
+    p++;
+  });
 }
